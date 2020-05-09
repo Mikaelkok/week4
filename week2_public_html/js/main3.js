be5048d16b4a154669271f4d0ca2a35fdbdfb328
Mikaelkok <49225413+Mikaelkok@users.noreply.github.com>
2020-05-09 19:26:00 +0300

    Update main3.js

--- week2_public_html/js/main3.js
+++ week2_public_html/js/main3.js
@@ -1,203 +1,82 @@
-'use strict';
-const url = 'http://localhost:3000'; // change url when uploading to server
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8">
+    <title>Example</title>
+    <link href="./css/style2.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="forms">
+        <div class="form-wrapper" id="login-wrapper">
+            <h3>Login</h3>
+            <form id="login-form" class="light-border">
+                <div>
+                    <label>Email:</label>
+                    <input class="light-border" type="text" name="username"/>
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input class="light-border" type="password" name="password"/>
+                </div>
+                <div>
+                    <button class="light-border"  type="submit">Log In</button>
+                </div>
+            </form>
+        </div>
+        <button class="light-border"  style="display: none;" id="log-out">Log out</button>
+    </div>
+    <h1>List of cats</h1>
+    <ul>
+        <!-- js creates list here -->
+    </ul>
+    <hr>
+    <div class="forms">
+        <div class="form-wrapper">
+            <h3>Add user (not working yet)</h3>
+            <form action="http://localhost:3000/user" method="post" class="light-border" id="add-user-form">
+                <input class="light-border" type="text" name="name" pattern=".{3,}" placeholder="Name" required>
+                <input class="light-border" type="email" name="email" placeholder="email" required>
+                <input class="light-border" type="password" name="passwd" pattern="(?=.*[A-Z]).{8,}"
+                       placeholder="Password" required>
+                <button class="light-border" type="submit">Save</button>
+            </form>
+        </div>
 
-// select existing html elements
-const loginWrapper = document.querySelector('#login-wrapper');
-const logOut = document.querySelector('#log-out');
-const loginForm = document.querySelector('#login-form');
-const addForm = document.querySelector('#add-cat-form');
-const modForm = document.querySelector('#mod-cat-form');
-const ul = document.querySelector('ul');
-const userLists = document.querySelectorAll('.add-owner');
+        <div class="form-wrapper">
+            <h3>Add cat</h3>
+            <form action="http://localhost:3000/cat"
+                  method="post"
+                  class="light-border"
+                  enctype="multipart/form-data"
+                  id="add-cat-form">
+                <input class="light-border" type="text" name="name" placeholder="Name" required>
+                <input class="light-border" type="number" name="age" placeholder="Age" required>
+                <input class="light-border" type="number" step="0.1" name="weight" placeholder="Weight" required>
+                <select class="light-border add-owner" name="owner" required>
+                    <option value="">Select owner</option>
+                </select>
+                <input class="light-border" type="file" name="cat" accept="image/*" placeholder="Choose file">
+                <button class="light-border" type="submit">Upload</button>
+            </form>
+        </div>
 
-// create cat cards
-const createCatCards = (cats) => {
-  // clear ul
-  ul.innerHTML = '';
-  cats.forEach((cat) => {
-    // create li with DOM methods
-    const img = document.createElement('img');
-    img.src = url + '/' + cat.filename;
-    img.alt = cat.name;
-    img.classList.add('resp');
-
-    const figure = document.createElement('figure').appendChild(img);
-
-    const h2 = document.createElement('h2');
-    h2.innerHTML = cat.name;
-
-    const p1 = document.createElement('p');
-    p1.innerHTML = `Age: ${cat.age}`;
-
-    const p2 = document.createElement('p');
-    p2.innerHTML = `Weight: ${cat.weight}kg`;
-
-    const p3 = document.createElement('p');
-    p3.innerHTML = `Owner: ${cat.ownername}`;
-
-    // add selected cat's values to modify form
-    const modButton = document.createElement('button');
-    modButton.innerHTML = 'Modify';
-    modButton.addEventListener('click', () => {
-      const inputs = modForm.querySelectorAll('input');
-      inputs[0].value = cat.name;
-      inputs[1].value = cat.age;
-      inputs[2].value = cat.weight;
-      inputs[3].value = cat.cat_id;
-      modForm.querySelector('select').value = cat.owner;
-    });
-
-    // delete selected cat
-    const delButton = document.createElement('button');
-    delButton.innerHTML = 'Delete';
-    delButton.addEventListener('click', async () => {
-      const fetchOptions = {
-        method: 'DELETE',
-        headers: {
-          'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
-        },
-      };
-      try {
-        const response = await fetch(url + '/cat/' + cat.cat_id, fetchOptions);
-        const json = await response.json();
-        console.log('delete response', json);
-        getCat();
-      } catch (e) {
-        console.log(e.message());
-      }
-    });
-
-    const li = document.createElement('li');
-    li.classList.add('light-border');
-
-    li.appendChild(h2);
-    li.appendChild(figure);
-    li.appendChild(p1);
-    li.appendChild(p2);
-    li.appendChild(p3);
-    li.appendChild(modButton);
-    li.appendChild(delButton);
-    ul.appendChild(li);
-  });
-};
-
-// AJAX call
-
-const getCat = async () => {
-  console.log('getCat token ', sessionStorage.getItem('token'));
-  try {
-    const options = {
-      headers: {
-        'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
-      },
-    };
-    const response = await fetch(url + '/cat', options);
-    const cats = await response.json();
-    createCatCards(cats);
-  } catch (e) {
-    console.log(e.message);
-  }
-};
-
-// create user options to <select>
-const createUserOptions = (users) => {
-  userLists.forEach((list) => {
-    // clear user list
-    list.innerHTML = '';
-    users.forEach((user) => {
-      // create options with DOM methods
-      const option = document.createElement('option');
-      option.value = user.user_id;
-      option.innerHTML = user.name;
-      option.classList.add('light-border');
-      list.appendChild(option);
-    });
-  });
-};
-
-// get users to form options
-const getUsers = async () => {
-  try {
-    const options = {
-      headers: {
-        'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
-      },
-    };
-    const response = await fetch(url + '/user', options);
-    const users = await response.json();
-    createUserOptions(users);
-  } catch (e) {
-    console.log(e.message);
-  }
-};
-
-// submit add cat form
-addForm.addEventListener('submit', async (evt) => {
-  evt.preventDefault();
-  const fd = new FormData(addForm);
-  const fetchOptions = {
-    method: 'POST',
-    headers: {
-      'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
-    },
-    body: fd,
-  };
-  const response = await fetch(url + '/cat', fetchOptions);
-  const json = await response.json();
-  console.log('add response', json);
-  getCat();
-});
-
-// submit modify form
-modForm.addEventListener('submit', async (evt) => {
-  evt.preventDefault();
-  const data = serializeJson(modForm);
-  const fetchOptions = {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
-    },
-    body: JSON.stringify(data),
-  };
-
-  console.log(fetchOptions);
-  const response = await fetch(url + '/cat', fetchOptions);
-  const json = await response.json();
-  console.log('modify response', json);
-  getCat();
-});
-
-// login
-loginForm.addEventListener('submit', async (evt) => {
-  evt.preventDefault();
-  const data = serializeJson(loginForm);
-  const fetchOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  };
-
-  const response = await fetch(url + '/auth/login', fetchOptions);
-  const json = await response.json();
-  console.log('login response', json);
-  if (!json.user) {
-    alert(json.message);
-  } else {
-    sessionStorage.setItem('token', json.token);
-    loginWrapper.style.display = 'none';
-    logOut.style.display = 'block';
-    getCat();
-    getUsers();
-  }
-});
-
-// check if token exists and hide login form, show logout button, get cats and users
-if (sessionStorage.getItem('token')) {
-  loginWrapper.style.display = 'none';
-  logOut.style.display = 'block';
-  getCat();
-  getUsers();
-}
+        <div class="form-wrapper">
+            <h3>Modify cat</h3>
+            <form action="http://localhost:3000/cat" id="mod-cat-form" class="light-border">
+                <input class="light-border" type="text" name="name" placeholder="Name" required>
+                <input class="light-border" type="number" name="age" placeholder="Age" required>
+                <input class="light-border" type="number" step="0.1" name="weight" placeholder="Weight" required>
+                <select class="light-border add-owner" name="owner" required>
+                    <option value="">Select owner</option>
+                </select>
+                <input type="hidden" name="id">
+                <button class="light-border" type="submit">Save changes</button>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="./js/serialize.js"></script>
+<script src="./js/main3.js"></script>
+</body>
+</html>
